f0311616096ab869224c2e020145cc7d17670bf4
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-28 17:54:44 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,56 @@
+class Song
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  @@all = {}
+
+  attr_accessor :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+
+    @@all[name] = {}
+    @@all[name]["artist"] = artist
+    @@all[name]["genre"] = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.uniq.each do |genre|
+      genre_count[genre] = @@genres.count(genre)
+    end
+    genre_count
+  end
+
+  def self.artist_count
+    artist_count = {}
+    @@artists.uniq.each do |genre|
+      artist_count[genre] = @@artists.count(genre)
+    end
+    artist_count
+  end
+
+end
