f6db51ac1be4d387cd055471ec3bf8bcc5fbfe10
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-28 17:30:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,12 +4,12 @@ class Song
   @@genres = []
 
   attr_accessor :name, :artist, :genre
-  
+
   def intialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
-    
+
     @@count += 1
     @@genres.include?(genre) ? nil : @@genres << genre
   end
